4114d0f3b15b2b029c2304151194942b5b930196
Daniel <karrunucu@gmail.com>
2019-12-11 13:05:03 +0000

    Update BoardStateModified.java

--- BoardStateModified.java
+++ BoardStateModified.java
@@ -1,17 +1,18 @@
 import java.util.Arrays;
 
 public class BoardState{
-    private Piece[] puzzle;
+    private static Piece[] puzzle;
     private final static int maxGuesses = 10;  
-    private Piece[] guess;
+    private static Piece[] guess;
     private Piece[][] board;
-    private int guesses = 0;
+    private int guesses = -1;
 
     public static boolean isValid(Piece[] pieces){
+        
         boolean isValid = true;
         int i = 0;
-        if(pieces.length == puzzleLength() && puzzleLength() > 1){
-            while(isValid || i < puzzleLength() ){
+        if(pieces.length == puzzle.length && puzzle.length > 1){
+            while(isValid || i < puzzle.length ){
                 if(pieces[i] != null ){
                     isValid = false;
                 }
@@ -50,35 +51,33 @@ public class BoardState{
        }
    }
 
-   public static int colorPosMatch(int i){
+   public int colorPosMatch(int i){
     int match = 0;
-    for(int i = 0; i < puzzleLength(); i++){ 
-      if(puzzle[i] == guess[i]){
+    for(int j = 0; j < puzzleLength(); j++){ 
+      if(puzzle[j] == guess[j]){
         match++;
       }
     }
     return match;
   }
 
-  public static int onlyColorMatches(int i){
+  public int onlyColorMatches(int i){
     int colorMatch = 0;
-    int[] puzzleCpy = Arrays.copyOf(puzzle, puzzleLength());
+    boolean[] puzzleBoolean = new boolean[puzzleLength()];
     for(int j = 0; j < puzzle.length; j++){ 
         if(puzzle[i] == guess[i] ){
-        puzzleCpy[i] = -400; 
+        puzzleBoolean[i] = true;
       }
     }
     for(int j = 0; j < puzzleLength(); j++){
       boolean colorM = true;
-      int j = 0;
       if(guess[i] != puzzle[i]){
         while(colorM && (j < puzzleLength())){ 
-          if(guess[i] == puzzleCpy[k]){
-            puzzleCpy[j] = -342;
+          if(guess[i] == puzzle[j] && !puzzleBoolean[j]) {
+            puzzleBoolean[j] = true;
             colorMatch++;
             colorM = false;
           }
-          j++;
         }
       }
     }
@@ -87,14 +86,14 @@ public class BoardState{
 
   public String toString(){
     System.out.print("+");
-    for(int j = 0; j < (2*board[i].length)+5; j++){
+    for(int j = 0; j < (2*board[guesses].length)+5; j++){
       System.out.print("-");
     }
     System.out.print("+\n");
-    for(int k = i; k >= 0; k--){
+    for(int k = guesses; k >= 0; k--){
       int match = colorPosMatch(guesses), colorMatch = onlyColorMatches(guesses);
       System.out.print("| ");
-      for(int n:board[k]){
+      for(Piece n:board[k]){
         System.out.print(n);
       }
       System.out.print(" | ");
@@ -104,12 +103,12 @@ public class BoardState{
       for(int j = 0; j < colorMatch; j++){
         System.out.print('*');
       }
-      for(int j = 0; j < board[i].length - (match + colorMatch); j++){
+      for(int j = 0; j < board[guesses].length - (match + colorMatch); j++){
          System.out.print(' ');
       }
       System.out.println(" |");
       System.out.print("+");
-      for(int j = 0; j < (2*board[i].length)+5; j++){
+      for(int j = 0; j < (2*board[guesses].length)+5; j++){
         System.out.print("-");
       }
       System.out.print("+\n");
