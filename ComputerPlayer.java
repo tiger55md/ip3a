5a593d7b52592dcc4d65e49182901fed0d4dee7d
Daniel <karrunucu@gmail.com>
2019-12-14 23:33:11 +0000

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,10 +1,20 @@
 public class ComputerPlayer{
-    private int puzzleLength = puzzleLength();
-    private int maxGuesses = maxGuesses();
-    private long seed = 7;
+    private int puzzleLength;
+    private int maxGuesses;
+    private long seed;
 
     public ComputerPlayer(int puzzleLength, int maxGuesses){
         this.puzzleLength = puzzleLength;
         this.maxGuesses = maxGuesses;
+    }
+
+    public ComputerPlayer(int puzzleLength, int maxGuesses, long seed){
+        this.puzzleLength = puzzleLength;
+        this.maxGuesses = maxGuesses;
+        this.seed = seed;
+    }
+
+    public Piece[] firstGuess{
+        
     }
 }
