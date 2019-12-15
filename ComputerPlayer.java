b245d69f73cc1294e2b70e88dc3c0b0df644014e
Daniel <karrunucu@gmail.com>
2019-12-15 22:17:24 +0000

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,7 +1,9 @@
-public class ComputerPlayer{
+import java.util.Random;
+
     private int puzzleLength;
     private int maxGuesses;
     private long seed;
+    private Piece[] pieces=Piece.values();
 
     public ComputerPlayer(int puzzleLength, int maxGuesses){
         this.puzzleLength = puzzleLength;
@@ -14,7 +16,12 @@ public class ComputerPlayer{
         this.seed = seed;
     }
 
-    public Piece[] firstGuess{
-        
+    public Piece[] firstGuess(){
+        Random rd = new Random();
+        Piece[] firstGuess  = new Piece[puzzleLength];
+        for(int i = 0; i < puzzleLength; i++){
+            firstGuess[i] = pieces[rd.nextInt(puzzleLength)];
+        }
+        return firstGuess;
     }
 }
