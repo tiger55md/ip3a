40aacab617f4042dc9a7fc0e072ecd1c3ee48b69
Daniel <karrunucu@gmail.com>
2019-12-20 15:51:51 +0000

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,8 +1,33 @@
 public enum Piece{
-		B,
-		Y,
-		W,
-		R,
-		O,
-		G
+    Blue,
+    Yellow ,
+    White,
+    Red,
+    Orange ,
+    Green ;
+
+    public String toString(){
+        String initial = " ";
+        switch(this){
+        case Blue :
+            initial = "B";
+            break;
+        case Yellow :
+            initial = "Y";
+            break;
+        case Red :
+            initial = "R";
+            break;
+        case Orange :
+            initial = "O";
+            break;
+        case White :
+            initial = "W";
+            break;
+        case Green :
+            initial = "G";
+            break;
+        }
+        return initial;
+    }
 }
